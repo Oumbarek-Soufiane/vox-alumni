a3d55405f06c5014daef050cd110cee6fcdad692
unknown <soufianeoump6@gmail.com>
2026-03-18 11:09:01 +0000

    Add migration route

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,12 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/run-migrations', function () {
+    try {
+        Artisan::call('migrate', ['--force' => true]);
+        return "✅ تمت إضافة الجداول بنجاح فـ TiDB! <br><br> Output: <pre>" . Artisan::output() . "</pre>";
+    } catch (\Exception $e) {
+        return "❌ وقع مشكل فالميݣراسيون: " . $e->getMessage();
+    }
+});
