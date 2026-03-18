140d27676cbcf6c4a6f986d61b2be888c5af9694
unknown <soufianeoump6@gmail.com>
2026-03-18 11:17:30 +0000

    force SSL for TiDB

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -9,9 +9,13 @@ class AppServiceProvider extends ServiceProvider
     /**
      * Register any application services.
      */
-    public function register(): void
+   public function register(): void
     {
-        //
+        // فرض التشفير (SSL) باش يقبلنا TiDB
+        config(['database.connections.mysql.options' => [
+            \PDO::MYSQL_ATTR_SSL_CA => '/etc/ssl/certs/ca-certificates.crt',
+            \PDO::MYSQL_ATTR_SSL_VERIFY_SERVER_CERT => false,
+        ]]);
     }
 
     /**
